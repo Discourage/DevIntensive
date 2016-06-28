ec633d24df6b956988304aedbd248c78d10cd0cd
Discourage <discourage73@yandex.ru>
2016-06-28 21:56:43 +0400

    hometask_3

---
+++ app/src/main/java/com/softdesign/devintensive/data/managers/DataManager.java
@@ -0,0 +1,22 @@
+package com.softdesign.devintensive.data.managers;
+
+public class DataManager {
+    private static DataManager INSTANCE = null;
+    private PreferenceManager mPreferenceManager;
+
+    public DataManager() {
+        this.mPreferenceManager = new PreferenceManager();
+    }
+
+    public static DataManager getINSTANCE() {
+        if (INSTANCE == null) {
+            INSTANCE = new DataManager();
+        }
+        return INSTANCE;
+    }
+
+    public PreferenceManager getPreferenceManager() {
+        return mPreferenceManager;
+    }
+
+}
--- app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -3,5 +3,12 @@ package com.softdesign.devintensive.utils;
 public interface ConstantManager {
     /*TAG FOR LOG*/
 
-    String TAG_PREFIX ="DEV";
+    String TAG_PREFIX = "DEV";
+    String EDIT_MODE_KEY = "EDIT_MODE_KEY";
+
+    String USER_PHONE_KEY = "USER_PHONE_KEY";
+    String USER_MAIL_KEY = "USER_MAIL_KEY";
+    String USER_VK_KEY = "USER_VK_KE";
+    String USER_GITHUB_KEY = "USER_GITHUB_KEY";
+    String USER_BIO_KEY = "USER_BIO_KEY";
 }
